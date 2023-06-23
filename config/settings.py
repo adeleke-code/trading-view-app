75812008029981a553b92cd04bc3b1f691a06cf4
Adeleke Femi <oluwafemiadeleke13@gmail.com>
2023-06-23 19:52:24 +0100

    allowed host

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = "sceretkey"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['trade-view-app-production.up.railway.app']
 
 
 # Application definition
